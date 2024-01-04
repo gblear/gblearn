c6b39aea6f8676ffabb474875e5c9bf5598fd2cb
gblear <matwey-golikow228@yandex.ru>
2024-01-04 19:06:57 +0300

    SOLUTION FROM SEMINAR 1

---
+++ seminar1task2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите 2 числа");
+string num1 = Console.ReadLine();
+string num2 = Console.ReadLine();
+
+int number1 = int.Parse(num1);
+int number2 = int.Parse(num2);
+
+if( number1 > number2 )
+{
+  Console.WriteLine("Первое число больше, чем второе");
+}
+else
+{
+    Console.WriteLine("Второе число больше, чем первое");
+}
---
+++ seminar1task4/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите 2 числа");
+string num1 = Console.ReadLine();
+string num2 = Console.ReadLine();
+string num3 = Console.ReadLine();
+
+int number1 = int.Parse(num1);
+int number2 = int.Parse(num2);
+int number3 = int.Parse(num3);
+
+if(number1 > number2)
+{
+    if(number1 > number3)
+    {
+        Console.WriteLine("1 число самое большое");
+    }
+    else
+    {
+        Console.WriteLine("3 число самое большое");
+    }
+}
+else
+{
+    if(number2 > number3)
+    {
+        Console.WriteLine("2 число самое большое");
+    }
+    else
+    {
+        Console.WriteLine("3 число самое большое");
+    }
+}
